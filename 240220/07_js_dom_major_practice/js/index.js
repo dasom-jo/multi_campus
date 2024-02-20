1e4e803542fdb1369a851b22ad19c227ac1b0a9d
jodasom <jodasom0216@gmail.com>
2024-02-21 08:49:18 +0900

    복습정리

--- 240220/07_js_dom_major_practice/js/index.js
+++ 240220/07_js_dom_major_practice/js/index.js
@@ -1,51 +1,39 @@
-//등록버튼을 눌럿을때 이름과 전공을 콘솔에  출력
+// 1. 등록버튼을 눌렀을 때, 이름과 전공을 콘솔에 출력 (성공)
 const addBtn = document.querySelector('button');
-let name = document.querySelector('#username').value;
-let major = document.querySelector('#major').value;
 
-let add = (e) => {
-    e.addBtn.addEventListener('click',(e)=>{
-    console.log(`${name}, ${major}`); //콘솔창에 찍히게만듬
-    e.preventDefault();//여기 e가잇기때문에 매개변수를 e로지정하면 좋음?
+let add = () => {    
+    let name = document.querySelector('#username'); // 홍길동
+    let major = document.querySelector('#major'); // 컴퓨터
+    // console.log(`${name}, ${major}`);
 
-    if(name && major) {//이값이 잇으면
+    if (name.value && major.value) {
+        // 2. <tr> <td>${name}</td><td>${major}</td> </tr>
+        let tr = document.createElement('tr'); // <tr></tr>
 
-    //2.tr td name td td major td tr
-    //html에 없으므로 여기서 요소 만들어줌
-let tr = document.createElement('tr');//<tr></tr>
+        let tdName = document.createElement('td'); // <td></td>
+        let nameText = document.createTextNode(name.value); // 텍스트 노드 : 홍길동
+        tdName.appendChild(nameText); // tdName = <td>홍길동</td>
 
-let tdName = document.createElement('td');//td 생성
+        let tdMajor = document.createElement('td'); // <td></td>
+        tdMajor.innerText = major.value; // tdMajor = <td>컴퓨터</td>
 
-let nameText = document.createTextNode(name);//텍스트 노드:홍길돌
-    tdName.appendChild(nameText); //입양 첫째 td홍길동td
+        tr.appendChild(tdName); // <tr><td>홍길동</td></tr>
+        tr.appendChild(tdMajor); // <tr><td>홍길동</td><td>컴퓨터</td></tr>
 
-let tdMajor = document.createElement('td');
-    tdMajor.innerText=major;//입양 방법2
-
-
-
-    tr.appendChild(tdName);//또 윗세대로 입양
-    tr.appendChild(tdMajor);
-
-    document.querySelector('#attendant > tbody').appendChild(tr); //입양
-    document.querySelector('#username').value = '';//입력후 초기화
-    document.querySelector('#major').value = '';
-    document.querySelector('#username').focus();
-    document.querySelector('#major').addEventListener('keydown') ,() =>{
-        console.log(e.code); }
-    //console.log(tr,tdName,tdMajor);
-
+        document.querySelector('#attendant > tbody').appendChild(tr);
+        name.value = '';
+        major.value = '';
+        document.querySelector('#username').focus();
     }
-})}
+}
 
-document.addEventListener('click',(e => {
-    e.prevenyDefault();
+addBtn.addEventListener('click', (e)=> {
+    e.preventDefault();
     add();
-}));
+});
 
 document.querySelector('#major').addEventListener('keyup', (e) => {
     if (e.code == "Enter") {
         add();
-    }})
-
-
+    }
+})
