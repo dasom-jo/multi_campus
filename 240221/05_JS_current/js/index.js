83224936a1cafd2ecb6ecd976d2e0197d8ed4d81
jodasom <jodasom0216@gmail.com>
2024-02-22 00:33:06 +0900

    복습정리

--- 240221/05_JS_current/js/index.js
+++ 240221/05_JS_current/js/index.js
@@ -1,55 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {//모든 DOM 컨텐트' 가 로드되었을 때, 실행된다.
 /*현재날짜 콘솔에 출력*/ 
-const today = new Date();
-//console.log(today);
-//console.log(today.toDateString());
-
+let today = new Date();
+console.log(today);
+console.log(today.toDateString());
 const week = ['일','월','화','수','목','금','토'];
-
-//조건이있을경우
-//let hour = today.getHours();
-//let am_pm = 'AM';
-//-----
-
-
-
-// today.setHours(12);
-/*const todayObj = {
-    year: today.getFullYear(),
-    month : today.getMonth() + 1,
-    date : today.getDate(),
-    day : week[today.getDay()]+'요일',
-    am_pm : today.getHours() <12 ? 'AM' : "PM",
-
-    hour : function () {
-        let hour = today.getHours()%12;
-        if(hour === 0) {
-            hour = 12
-        } else { 
-            hour 
-        }
-        return 12
-    },
-
-    min : today.getMinutes() <10 ? '0'+ getMinutes() : getMinutes(),
-    sec : today.getSeconds() <10 ? '0' + today.getSeconds() : today.getSeconds()
-
-}
-
-
-document.addEventListener('DOMContentLoaded',()=> {
-    const todayDiv = document.querySelector('#today')
-    todayDiv.innerHTML = `
-    ${todayObj.year}년 ${todayObj.month}월 ${todayObj.date}일 <span>${todayObj.day}<span>`
-
-    const clockDiv = document.querySelector('clock');
-    clockDiv.innerHTML = `
-    ${todayObj.am_pm}  ${todayObj.hour} : ${todayObj.min} : ${todayObj.sec}`
-
-
-})*/
 
 
 //실시간으로 바뀌는것
+function clock() {
+    console.log(today.getMilliseconds());//실시간 업뎃
+    today = new Date();//현재 시간을 저장하고 로그에 전달
+
+
 
 
 const todayObj = {
@@ -57,14 +19,14 @@ const todayObj = {
     month : today.getMonth() + 1,
     date : today.getDate(),
     day : week[today.getDay()]+'요일',
-    am_pm : today.getHours() <12 ? 'AM' : "PM",
+    am_pm : today.getHours() < 12 ? 'AM' : "PM",
 
     hour : function () {
-        let hour = today.getHours()%12;
+        let hour = today.getHours() % 12;
         if(hour === 0) {
             hour = 12
-        } else { 
-            hour ;
+        } else if(hour < 10) { 
+            hour = "0" + hour ;
         }
         return hour;
     },
@@ -75,11 +37,16 @@ const todayObj = {
 }
 
 
-document.addEventListener('DOMContentLoaded',()=> {
+
     const todayDiv = document.querySelector('#today')
     todayDiv.innerHTML = `
-    ${todayObj.year}년 ${todayObj.month}월 ${todayObj.date}일 <span>${todayObj.day}<span>`
+    ${todayObj.year}년 ${todayObj.month}월 ${todayObj.date}일 <span>${todayObj.day}<span>`;
 
     const clockDiv = document.querySelector('#clock');
-    clockDiv.innerHTML = `
-    ${todayObj.am_pm}  ${todayObj.hour} : ${todayObj.min} : ${todayObj.sec}` })
+    clockDiv.innerText = `
+    ${todayObj.am_pm}  ${todayObj.hour()} : ${todayObj.min} : ${todayObj.sec}` 
+
+}
+
+setInterval(clock,1000);
+})
--- 240221/08_js_change_bg/js/index.js
+++ 240221/08_js_change_bg/js/index.js
@@ -1,12 +1,14 @@
 window.addEventListener('load',() =>{
-    const bgcount = 5;
-    let rendomNum = 0; //1부터 5까지 랜덤 숫자
+    // const bgcount = 5;
+    // let randomNum = Math.floor(Math.random() * bgcount) + 1
+    // //console.log(randomNum);
+    // document.body.style.backgroundImage = `url(./images/bg-${randomNum}.jpg)`;
 
-    let randomNum = Math.floor(Math.random() * bgcount) + 1
-    //console.log(randomNum);
-
-    document.body.style.backgroundImage = `url(./images/bg-${randomNum}.jpg)`;
+    //다른방법
+    const imgArr = ['bg-1.jpg', 'bg-2.jpg', 'bg-3.jpg', 'bg-4.jpg', 'bg-5.jpg'];
+    let randomNum = Math.floor(Math.random() * imgArr.length);
+    document.body.style.backgroundImage = `url(./images/${imgArr[randomNum]})`
 })  
 
 
-//다른방법
+
