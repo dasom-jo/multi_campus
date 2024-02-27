e2ae3ec2930c160b872540edad6459fd1fb1d9bd
jodasom <jodasom0216@gmail.com>
2024-02-27 23:17:54 +0900

    복습정리

--- 240223/05_js_reduce/js/index.js
+++ 240223/05_js_reduce/js/index.js
@@ -1,20 +1,27 @@
-//reduce
-// 배열의 각요소에대해 주어진 함수를 실행하고, 하나의 결과값을 반환
-//forEach(f(요소,인덱스,배열))
-//map(f(요소,인덱스,배열))
-//filter(f(요소,인덱스,배열))
-//reduce((누산기,요소,인덱스,배열)=>{},초기값)
-let total = nums.reduce((tot, num) =>{ 
-    console.log(tot, num);
-    return tot + num} , 5
-);
+/*reduce*/
+//배열의 각 요소에 대해 주어진 함수를 실행하고, "하나의 결과값을 반환"
+
+let nums = [1,2,3,4,5];
+
+/*let total = 0;
+nums.forEach(num => total += num);
+console.log(total);*/
+
+//forEach((요소,인덱스,배열)=>{})
+//map((요소,인덱스,배열)=>{})
+//filter((요소,인덱스,배열)=>{})
+//reduce((누산기,요소,인덱스,배열)=>{},초기값) / 이전요소들의 결과를 누적하여 반환
+
+let total = nums.reduce((tot,num)=>{
+    console.log(tot,num);
+    return tot +num;
+},0);
 console.log(total);
+//tot는 누적값//초기값으로 시작
 
-
-
-let strArr = ["Hello","Nice","Javascript"];   
-let result = strArr.reduce((tot, str)=> [...tot,str.length],[] );
-console.log(result);
-
-
-//...acc,str.length =[].concat[5]
+let strArr = ['hello','nice','javascript'];
+//let result = strArr.reduce((acc,str)=> acc.concat(str.length),[]);
+let result = strArr.reduce((acc, str) => [...acc, str.length],[]);
+console.log(result); 
+//[5,4,10] reduce를 사용해서 문자열의 길이를 배열로 반환 
+//[]빈 배열이 공백
--- 240223/17_js_xhr_prati/js/index.js
+++ 240223/17_js_xhr_prati/js/index.js
@@ -1,35 +1,36 @@
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET',"./data/student.json");
-xhr.send();
+let xhr = new XMLHttpRequest(); //선언하기
+xhr.open('GET',"./data/student.json"); //제이슨받기
+xhr.send(); //보내기
 const result = document.querySelector("#result");
 xhr.onreadystatechange=()=> {
     
     if(xhr.readyState === 4 && xhr.status ===200){
-        let student = JSON.parse(xhr.responseText);
-
-        for(let i = 0; i <16; i++ ){
-        result.innerHTML+=`<ul class=box>
-                        <li class= name>행운의 컬러: ${student[i].color}</li><br>
-                        <li class= name>행운의 숫자: ${student[i].number}</li><br>
-                        <li class= name>행운의 아이템: ${student[i].item}</li>
-                        </ul>`
-        }
-        console.log(student)
-        
-    };
-}
-
-//랜덤함수
-//뽑기를 누르면 여러 카드중 한장 카드만 색이바뀌거나 뽑힌다
-//닫기를 누르면 원래대로
-
+        let cards = JSON.parse(xhr.responseText);
+    
 
 // function random(min, max) {
 //     const num = Math.floor(Math.random() * (max - min)) + min;
 //     return num;
 // }
 
+    let randomSet = new Set([]);//빈공간으로 놔둔이유는 랜덤함수의 판을 까는것 
+    while (randomSet.size !=1) { //뽑고 싶은 인덱스의 갯수
+        const randomIdx = Math.floor(Math.random() * cards.length);//어디범위까지 인덱스를 지정할지
+        randomSet.add(cards.splice(randomIdx, 1)[0]);//지정된 인덱스중에서 이미 1개의 인덱스르뽑기를 원햇으므로 \
+        //1개의 인덱스를 삭제하기위해 splice 를 넣고 제로베이스가된 상태에서  0이라는 첫번째 빈요소칸에 randomSet을 입력하는것이다
+    }//splice(start,delete  Count)
+    for (let card of randomSet) {
+        result.innerHTML += `<ul class=box>
+                            <li class = name>행운의 컬러: ${card.color}</li><br>
+                            <li class = name>행운의 숫자: ${card.number}</li><br>
+                            <li class = name>행운의 아이템: ${card.item}</li><br>
+                            </ul>`
+    }
+    console.log(cards)
+        
+    };
+}
 
 /*버튼클릭시 작동*/ 
 let openbtn = document.querySelector('#openbtn');
