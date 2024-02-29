4502165fa3707d077a49e058c03a53c132561973
jodasom <jodasom0216@gmail.com>
2024-03-01 00:06:46 +0900

    복습자료

--- 240226/02_json_server/js/index.js
+++ 240226/02_json_server/js/index.js
@@ -1,41 +1,40 @@
-
-
-fetch('http://localhost:3000/contacts')
-    .then(Response => Response.json())
-    .then(result => {
-        console.log(result);
-                    const tbody =  document.querySelector('#contact')
-                    
-                    for(let contact of result) {
-                        let row = document.createElement('tr');
-                        // let htmlString = `
-                        // <td>${contact.id}</td>
-                        // <td>${contact.name}</td>
-                        // <td>${contact.tel}</td>
-                        // <td>${contact.address}</td>
-                        // <td><button>삭제</button></td>;
-                        // `
-                        // row.innerHTML=htmlString;
-                        // tbody.appendChild(row);
-                        let id_col = document.createElement('td')
-
-                    
-                    }}); //전체조회
-            
-
-
-    document.querySelector('#addBtn').addEventListener('click',()=>{
-        const id = document.querySelector('#id').value;
-        const name = document.querySelector('#name').value;
-        const tel = document.querySelector('#tel').value;
-        const addr = document.querySelector('#addr').value;
-
-        const newContact = { id,name, tel, address };
-
-        fetch('http://localhost:3000/contacts',{
-            method: 'POST',
-            headers:{'content-Type' : 'application/json'},
-            body: JSON.stringify(newContact)}
-            .then(Response => console.log(Response)))
-    });//추가하라고 요청보내느것
-
+function getDate() {
+    fetch('http://localhost:3000/contacts')
+        .then(Response => Response.json())
+        .then(result => {
+            console.log(result);
+            const tbody = document.querySelector('#contacts');
+            tbody.innerHTML = '';
+            for(let contact of result){
+                let row = document.createElement('tr');
+                let id_col = document.createElement('td');
+                id_col.innerText = contact.id;
+                let name_col = document.createElement('td');
+                name_col.innerText = contact.name;
+                let tel_col = document.createElement('td');
+                tel_col.innerText = contact.tel;
+                let addr_col = document.createElement('td');
+                addr_col.innerText = contact.address;
+                let del_col = document.createElement('td');
+                let del_btn = document.createElement('button');
+                del_btn.innerText = '삭제';
+                del_btn.addEventListener('click',()=>{
+                    console.log(contact.id);
+                    deleteDate(contact.id);
+                })
+                del_col.appendChild(del_btn);
+                let mod_col = document.createElement('td');
+                let mod_btn = document.createElement('button');
+                mod_btn.innerText = '수정';
+                mod_col.appendChild(mod_btn);
+                mod_btn.addEventListener('click',()=>{
+                    let newContact = {
+                        name: '최인규',
+                        tel: '010-0000--000',
+                        address: '미국'
+                    }
+                    modifyDate(contact.id, newContact)
+                })
+            }
+        })
+}
