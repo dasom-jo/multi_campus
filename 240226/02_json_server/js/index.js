aa269c164f19cebf713c69755204f690b1c9ffba
jodasom <jodasom0216@gmail.com>
2024-03-03 16:02:18 +0900

    자료정리

--- 240226/02_json_server/js/index.js
+++ 240226/02_json_server/js/index.js
@@ -1,14 +1,14 @@
 function getDate() {
     fetch('http://localhost:3000/contacts')
-        .then(Response => Response.json())
+        .then(Response => Response.json())//서버로부터 받은 응답
         .then(result => {
             console.log(result);
             const tbody = document.querySelector('#contacts');
-            tbody.innerHTML = '';
+            tbody.innerHTML = '';//새로운 html 생성
             for(let contact of result){
                 let row = document.createElement('tr');
                 let id_col = document.createElement('td');
-                id_col.innerText = contact.id;
+                id_col.innerText = contact.id;//id 선언
                 let name_col = document.createElement('td');
                 name_col.innerText = contact.name;
                 let tel_col = document.createElement('td');
@@ -35,6 +35,66 @@ function getDate() {
                     }
                     modifyDate(contact.id, newContact)
                 })
+                row.appendChild(id_col);
+                row.appendChild(name_col);
+                row.appendChild(tel_col);
+                row.appendChild(addr_col);
+                row.appendChild(del_col);
+                row.appendChild(mod_col);
+                tbody.appendChild(row);
             }
-        })
+        });
 }
+
+function modifyDate(id, newObj){
+    fetch('http://localhost:3000/contacts/' + id, {
+        method: 'PUT',
+        hearders : {'content-Type' : 'application/json'},
+        body: JSON.stringify(newObj)
+    }).then(response =>{
+        console.log(response);
+            if (response.ok) {
+                getDate();
+            }else { 
+                throw new Error('수정에러')
+        }
+    }).catch(e => console.error(e))
+}
+
+function deleteDate(id) {
+    fetch('http://localhost:3000/contacts/' + id, { method : 'DELETE'})
+        .then(response => {
+            console.log(response);
+            if(response.ok) {
+                getDate();
+            }else {
+                throw new Error('삭제에러')
+            }
+        }).catch(e => console.error(e))
+}
+
+function createDate(obj) {
+    fetch("http://localhost:3000/contacts", {
+        method : 'POST',
+        headers: {'content-Type': 'application/json'},
+        body: JSON.stringify(obj)
+    }).then(response => {
+        console.log(response);
+        if(response.ok){
+            getDate();
+        }else {
+            throw new Error('등록에러')
+        }
+    }).catch(e=> console.error(e))
+}
+
+getDate();
+
+document.querySelector('#addBtn').addEventListener('click', ()=>{
+    const name = document.querySelector('#name').value;
+    const tel = document.querySelector('#tel').value;
+    const address = document.querySelector*('#addr').value;
+
+    const newContact = {name, tel, address};
+    createDate(newContact);
+})
--- 240226/04_js_kakao/js/index.js
+++ 240226/04_js_kakao/js/index.js
@@ -1,98 +1,78 @@
-var container = document.querySelector('#map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표. /위도경도
-	level: 3 //지도의 레벨(확대, 축소 정도)
+let container = document.getElementById('map');//지도를 담을 영역의 DOM레퍼런스
+let options = { //지도를 생성할때 필요한 기본옵션 
+    center : new kakao.maps.LatLng(37.5025398, 127.0248679),// 지도의 중심좌표
+    level: 14 //지도의 레벨(확대,축소 정도)
 };
 
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+let map = new kakao.maps.Map(container,options); //지도 생성및 객체리턴
 
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-    mapOption = { 
-        center: new kakao.maps.LatLng(37.5025398, 127.0248679), // 지도의 중심좌표
-        level: 14 // 지도의 확대 레벨
-    };
 
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-// 마커가 표시될 위치입니다 
-var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
-
-// // 마커를 생성합니다
-// var marker = new kakao.maps.Marker({
-//     position: markerPosition
-// });
-
-// // 마커가 지도 위에 표시되도록 설정합니다
-// marker.setMap(map);
-
-// // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
-// // marker.setMap(null);   
-
-//마커 클러스트 설정
-let clusterer = new kakao.map.MarkerClusterer({
+//마커클러스터 설정 :가까이있는 마커들은 멀리서보면 하나의 마커로 보이게하는게하는거
+let clusterer = new kakao.maps.MarKerClusterer({
     map,
-    averageCenter : true,
-    minLevel :10,
+    averageCenter: true,
+    minLevel: 10
 })
 
-
-let servicekey = "yAVzxpkULGQRrM5zy4zu8iDmxlYO2S0lyY%2FkF6QV996eDh3eKBzOgM0j7YZwpW%2F%2BECYL4OkhcMNsVYswLD8TwQ%3D%3D0";
-let mobileOS = 'ETC'
-let MobileApp= 'AppTest'
-let url =  'http://apis.data.go.kr/B551011/GoCamping/basedList'
+let serviceKey = 'yAVzxpkULGQRrM5zy4zu8iDmxlYO2S0lyY%2FkF6QV996eDh3eKBzOgM0j7YZwpW%2F%2BECYL4OkhcMNsVYswLD8TwQ%3D%3D0';
+let mobileOS = 'ETC';
+let mobileApp = 'appName';
+let url = 'http://apis.data.go.kr/B551011/GoCamping/basedList';
 url += '?'
-url += `MobileOS=${mobileOS}&`;
-url += `MobileApp =${MobileApp}&`;
-url += `serviceKey = ${serviceKey}}&`;
+url += `MobileOs=${mobileOS}&`;
+url += `MobileApp=${moblieApp}$`;
+url += `serviceKey=${serviceKey}`;
 url += '_type=json';
 
 fetch(url)
     .then(response => response.json())
     .then(data => {
-        //캠핑장 데이터 배열 가져오기
-        const campingDate = data.response.body.items.item;
+        //캠핑장에서 배열 가져오기
+        const campingData = data.response.body.items.item;
 
         //테이블에 출력하기
         const table = document.querySelector('table');
         let contents = ``;
-        let markers=[];
-        campingDate.forEach((camp, index) => {
+        let marKers = [];
+        campingData.forEach((camp,index)=> {
             const mapSetting = makeMapSetting(camp);
-            let contents = ``;
-            campingDate.forEach((camp, index)=> {
-                const mapSetting = makeMapSetting(camp);
-                markers.push(mapSetting, )
-            })
+            marKers.push(mapSetting.marker);
+            kakao.maps.event.addListener(mapSetting.marker, 'mouseover', makeOverListener(map,mapSetting.marker,mapSetting.infowindow))
+            kakao.maps.event.addListener(mapSetting.marker, 'mouseout', makeOutListener(mapSetting.infowindow));
 
             contents += `
-                <tr>
-                    <td>${index + 1}</td>
-                    <td>${camp.facltNm}</td>
-                </tr>`
+                    <tr>
+                        <td>${index + 1}</td>
+                        <td>${camp.facltNm}</td>
+                    </tr>
+                `
         });
-        clusterer.addMarkers(marKers);
+        clusterer.addMarkers(markers);
         table.innerHTML = contents;
     })
 
-
 function makeMapSetting(item) {
     const obj = {
-        marker : new kakao.maps.Marker ({
+        marker: new kakao.maps.Marker({
             map,
-            position: new kakao.maps.LatLng()
+            position: new kakao.maps.LatLng(item.mapY, item.mapX),
+        }),
+        infowindow: new kakao.maps.Infowindow({
+            content : item.facltNm
         })
     }
+    return obj;
 }
 
-
-// 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+//인포윈도우를 표시하는 클로저를 만드는 함수입니다.
+//미커를 클릭햇을떄 표시되는 팝업창
 function makeOverListener(map, marker, infowindow) {
-    return function () {
-        infowindow.open(map, marker);
+    return function() {
+        infowindow.open(map,marker);
     };
 }
 
-// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+//인포윈도으룰 닫는 클로저를 만드는 함수입니다
 function makeOutListener(infowindow) {
     return function () {
         infowindow.close();
