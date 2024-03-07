31a64be1c5d1b0851473d20828f403fe52321328
Inkyu24 <484342@gmail.com>
2024-03-07 09:40:13 +0900

    0307

---
+++ 03_python/240307/00_func_type.py
@@ -0,0 +1,14 @@
+# 친절한 함수 만들기
+def plus(a:int, b:int) -> int:
+    """두 개의 정수를 합쳐서 반환한다.
+
+    Args:
+        a (int): 첫 번째 정수야
+        b (int): 두 번째 정수야
+
+    Returns:
+        int: 합한 값을 반환한다.
+    """
+    return a + b
+
+help(plus)
