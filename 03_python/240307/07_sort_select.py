7609eb32970d7d261eec133c913808a744d3afd2
Inkyu24 <484342@gmail.com>
2024-03-07 16:23:09 +0900

    ms

--- 03_python/240307/07_sort_select.py
+++ 03_python/240307/07_sort_select.py
@@ -1,13 +1,16 @@
 def selection_sorted(list):
+    count = 0
     for i in range(len(list) - 1):
         min = i
-        for j in range(i + 1, len(list)):
+        for j in range(i + 1, len(list)):            
+            count += 1
             if list[min] > list[j]:
                 min = j
         list[min], list[i] = list[i], list[min]
         # print(list)
+    print(count)
     return list
 
-num_list = [5, 4, 2, 6, 3, 1]
+num_list = [1,2,3,4,5,6,7]
 new_list = selection_sorted(num_list)
 print(new_list)
---
+++ 03_python/240307/09_sort_merge.py
@@ -0,0 +1,35 @@
+def merge_sorted(list):
+    length = len(list)
+    if length <= 1 :
+        return
+    mid = length // 2
+    group1, group2 = list[:mid], list[mid:]
+    merge_sorted(group1)
+    merge_sorted(group2)
+    
+    idx, idx1, idx2 = 0, 0, 0
+    
+    while idx1 < len(group1) and idx2 < len(group2):
+        if group1[idx1] < group2[idx2]:
+            list[idx] = group1[idx1]
+            idx1 += 1
+            idx += 1
+        else:
+            list[idx] = group2[idx2]
+            idx2 += 1
+            idx += 1
+    while idx1 < len(group1):
+        list[idx] = group1[idx1]
+        idx1 += 1
+        idx += 1
+    while idx2 < len(group2):
+        list[idx] = group2[idx2]
+        idx2 += 1
+        idx += 1
+    return list
+
+
+# 0 1 3 6 10 15 21 28 ... n(n+1)/2 [ O(n ** 2) ]
+num_list = [1, 2, 3, 4, 5, 6, 7]
+new_list = merge_sorted(num_list)
+print(new_list)
