2478910baf1ac7aea85d27bacf8a8e3cdc58f085
jodasom <jodasom0216@gmail.com>
2024-03-13 17:06:13 +0900

    수업자료

--- 240313/08_two_pointers.py
+++ 240313/08_two_pointers.py
@@ -6,7 +6,15 @@ rom typing import List
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         lt,rt = 0,len(nums) - 1 #구조분해 할당
-
+        [1,2,3,4,5]
+        while lt < rt:
+            temp_sum = nums[lt] + nums[rt]
+            if target == temp_sum:
+                return [lt,rt]
+            elif target < temp_sum:
+                rt -=1
+            elif target > temp_sum:
+                lt +=1
 s = Solution()
 result = s.twoSum([2,7,6,10],9)
 print(result)
