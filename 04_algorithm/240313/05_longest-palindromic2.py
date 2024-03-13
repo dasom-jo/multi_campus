671c2f2355dbd71e45fc031169da1aeddbf8b916
Inkyu24 <484342@gmail.com>
2024-03-13 13:49:29 +0900

    ag

--- 04_algorithm/240313/05_longest-palindromic2.py
+++ 04_algorithm/240313/05_longest-palindromic2.py
@@ -1,31 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def oddCheck(i):
-            left = i
-            right = i+2
+        def evenCheck(idx):
+            left = idx
+            right = idx + 1
             while left >= 0 and right <= s_length-1 and s[left] == s[right]:
                 left -= 1
                 right += 1
             return s[left+1:right]
-        
-        def evenCheck(i):
-            left = i
-            right = i+1
+        def oddCheck(idx):
+            left = idx
+            right = idx + 2
             while left >= 0 and right <= s_length-1 and s[left] == s[right]:
                 left -= 1
                 right += 1
             return s[left+1:right]
-                
         result = ""
         s_length = len(s)
         if len(s) < 2 or s == s[::-1]:
             return s
         for i in range(s_length - 1):
-            print(evenCheck(i))
-            print(oddCheck(i))
+            # print(result, evenCheck(i), oddCheck(i))
+            result = max(result, evenCheck(i), oddCheck(i), key=len)
         return result
             
                 
 sol = Solution()
-s = "김김최최김"
-sol.longestPalindrome(s)
+s = "cbbd"
+result = sol.longestPalindrome(s)
+print(result)
