8b8ac886e19afcca956781928fb1be6172fda9b9
jodasom <jodasom0216@gmail.com>
2024-03-20 21:26:10 +0900

    복습자료.힘들다..잘할수있으려나

--- 240315/11_1182.py
+++ 240315/11_1182.py
@@ -1,25 +1,26 @@
-#
 import sys
 
-# N,S = [int(i) for i in sys.stdin.readline().split()]
-# arr = [int(i) for i in sys.stdin.readline().split()]
-
-N,S = [5, 0]
-arr = [-7,-3,-2,5,8]
+N, S = [int(i) for i in sys.stdin.readline().split()]
+arr = [int(i) for i in sys.stdin.readline().split()]
 
 result = 0
-
-#arr와 s값 사용
-def dfs(idx,sum):
+# 5 0
+# -7 -3 -2 5 8
+# 1
+def dfs(idx, sum):
     global result
     if idx >= N:
         return
     sum += arr[idx]
+    # print(sum)
     if sum == S:
-        result +=1
-    dfs(idx+1,sum)
-    dfs(idx+1,sum - arr[idx])
+        result += 1
+    dfs(idx+1, sum)
+    dfs(idx+1, sum - arr[idx])
+    print(sum)
+    #두 가지 재귀 호출을 통해 현재 인덱스를
+    # 포함하거나 포함하지 않는 두 가지 경우를
+    # 모두 탐색하여 모든 가능한 부분집합을 만들고 합을 계산합니다.
 
-dfs(0,0)
+dfs(0, 0)
 print(result)
-
--- 240315/13_2178.py
+++ 240315/13_2178.py
@@ -1,7 +1,27 @@
 #너비전 탐색 :최소최단기간
 from collections import deque
+import sys
 
 N,M = [int(i) for i in sys.stdin.readline().split()]
+maze = [[int(i) for i in sys.stdin.readline().rstrip()] for _ in range(N)]
+#maze = []
+print(maze)
 
+direction = [(-1, 0),(1, 0),(0, -1),(0, 1)] #이동방향표시,튜플
 
+queue = deque()
+queue.append([0,0]) #시작위치
+while queue:
+    x, y = queue.popleft()
+    for d in direction:
+        #이동중
+        new_x, new_y = x + d[0], y + d[1] #0은 x축 1 y축
+        if 0 <= new_x < M and 0 <= new_y < N:
+            #해당위치가 1이면 이동가능한공간
+            if maze[new_y][new_x] == 1:
+                #새로운 위치 업데이트
+                maze[new_y][new_x] = maze[y][x] + 1
+                #새로업데이트한 위치를 추가하여 거기부터 재탐색
+                queue.append([new_x, new_y])
 
+print(maze[N-1][M-1])
