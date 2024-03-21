d6b5229410939655fe2c520e9fd531a8b556001a
jodasom <jodasom0216@gmail.com>
2024-03-21 16:41:07 +0900

    수업자료

--- 240321/07_num_to_word.py
+++ 240321/07_num_to_word.py
@@ -5,19 +5,23 @@
 class Solution:
     def num_to_word(self, n:int) -> str:
         nums_dict = {
-        #100_000_000 : '억', 10_000 : '만', 1_000: '천',
-        100 : '백',10 : '십',9: '구', 8: '팔', 7: '칠', 6: '육', 5: '오', 4: '사', 3: '삼', 2: '이', 1: '일'
+        100_000_000 : '억', 10_000 : '만', 1_000: '천',100 : '백',
+        10 : '십',9: '구', 8: '팔', 7: '칠', 6: '육', 5: '오', 4: '사', 3: '삼', 2: '이', 1: '일'
         }
+        if n == 0:
+            return "영"
         result = ""
-        for key, kor in nums_dict.items(): #여기 목이 9면 십 지나고 9에서 또비교들어감 계속 반복돌아감
+        for key, kor in nums_dict.items(): #원래값 들어가고 함수식돌다가 다시 여기 몫이 들어올때 9가나오면 십 지나고 9에서 또비교들어감 계속 그아래까지 불편하게 반복돌아감
             print(key,kor,n)
+            #if n == 0:
+                #break (두번째방법)
             #만약 입력한 n이 크거나
             if n >= key:
                 #몫
                 m = n // key
                 #n을 나머지값으러 변경
                 n = n % key
-                if n == 0:
+                if n == 0: #나머지가 딱 떨어져 0이되면 불필요하게 아래거까지 돌필요없이 브레이크
                     result += kor
                     break
                 #몫이 1보다 크면 (일억->억 )
@@ -29,4 +33,4 @@ class Solution:
         return result
 
 s = Solution()
-print(s.num_to_word(99))
+print(s.num_to_word(11))
