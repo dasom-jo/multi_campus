6d754eaa80b95f1db2c77991a8bc6e3de9e87416
Inkyu24 <484342@gmail.com>
2024-03-22 11:15:11 +0900

    tab

--- 04_algorithm/240322/03_pascals-triangle_bu.py
+++ 04_algorithm/240322/03_pascals-triangle_bu.py
@@ -1,11 +1,12 @@
 class Solution:
     def generate(self, numRows: int) -> list[list[int]]:
-        table = [[0] * (i + 1) for i in range(numRows)]
-        for i in range(len(table)):
+        table = [[1], [1, 1]]
+        table += [[0] * (i+1) for i in range(2, numRows)]
+        for i in range(2, numRows):
             table[i][0] = 1
             table[i][-1] = 1
-        
-        
+            for j in range(1, i):
+                table[i][j] = table[i-1][j-1] + table[i-1][j]
         return table
 
 s = Solution()
