ef8bef0a045f2fcd0f9d29c5efd069a81ddafb80
jodasom <jodasom0216@gmail.com>
2024-03-28 16:45:06 +0900

    복습

--- 240215/10_js_while_practice/js/index.js
+++ 240215/10_js_while_practice/js/index.js
@@ -1,9 +1,26 @@
-let i = 1
-let num = Number (prompt('숫자를 입력하세요'));
-    
-while ( i <= num){
+// while (조건) {
+//     반복;
+//     조건해제;
+// }
+
+let i = 1;
+let num = Number(prompt('입력하세요'));
+while (i <= num) {
     console.log(i);
     i++;
-    }
+}
 
 
+/* 모범 답안 */
+// let i = prompt("숫자를 입력하세요");
+
+// if (isNaN(i)) {
+//     console.log("유효한 숫자를 입력하세요.");
+// } else {
+//     let count = 1;
+
+//     while (count <= i) {
+//         console.log(count);
+//         count++;
+//     }
+// }
--- 240215/14_js_function_calc/js/index.js
+++ 240215/14_js_function_calc/js/index.js
@@ -7,7 +7,7 @@ function calcSum1() {
     document.write(`${result}`)
 }
 
-// 매개변수두개 (start, end)를 이용해서 start부터 end까지의 합계를 
+// 매개변수두개 (start, end)를 이용해서 start부터 end까지의 합계를
 //구하는 함수를 정의
 function calcSum2(start, end) {
 let result = 0;
@@ -15,10 +15,8 @@ for(let i = start; i<=end; i++){
     result +=i
 }
     document.write(`${start}부터 ${end} 까지의 합계는 ${result}입니다`)
-} 
+}
 
 calcSum1();
 document.write(`<br/>`);
 calcSum2(51,100);
-
-
