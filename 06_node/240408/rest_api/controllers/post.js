7e0ed161b0fea02c1fd598223c52fb3ed86fca90
Inkyu24 <484342@gmail.com>
2024-04-08 15:33:35 +0900

    a

--- 06_node/240408/rest_api/controllers/post.js
+++ 06_node/240408/rest_api/controllers/post.js
@@ -1,8 +1,16 @@
 const Post = require("../models/post");
+const User = require("../models/user");
+const Hashtags = require("../models/hashtag");
 
 exports.getPosts = async (req, res, next) => {
     try {
-        const posts = await Post.findAll({});
+        const posts = await Post.findAll({
+            include: {
+                model: User,
+                attributes: ["id", "nickname"],
+            },
+            order : [["createAt", "DESC"]]
+        });
         res.json({
             code: 200,
             payload: posts
@@ -21,6 +29,19 @@ exports.uploadPost = async (req, res, next) => {
             img: req.body.url,
             UserId: req.body.userId
         });
+        const hashtags = req.body.content.match(/#[^\s#]*/g)
+        if (hashtags) {
+            const result = await Promise.all(
+                hashtags.map(tag=> {
+                    return Hashtags.findOrCreate({
+                        where: {
+                            title: tag.slice(1).toLowerCase()
+                        }
+                    });
+                })
+            );
+            await post.addHashtags(result.map(r=>r[0]));
+        }
         res.json({
             code: 200,
             payload: post
