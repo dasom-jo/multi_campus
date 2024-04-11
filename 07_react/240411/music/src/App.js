9164ca99d1e87de0f8a1f1d5779ba9ef2232c08f
Inkyu24 <484342@gmail.com>
2024-04-11 17:44:21 +0900

    a

--- 07_react/240411/music/src/App.js
+++ 07_react/240411/music/src/App.js
@@ -8,6 +8,8 @@ function App() {
   // [과제2] : like라는 값을 통해서, 좋아요가 증가되도록 기능을 추가해보세요
   // [과제3] : 음악 변경 기능을 음악 추가 기능으로 변경해보세요
   // [과제4] : 좋아요가 개별적으로 적용되도록 수정해보세요
+  // [과제5] : 반복되는 요소를 컴포넌트로 분리하세요
+  // [과제6] : 수정, 삭제 기능을 추가해주세요
   const [musicList, setMusicList] = useState([
     {
       id: new Date().getTime(),
@@ -15,6 +17,23 @@ function App() {
       like: 0
     }
   ]);
+
+  const addMusic = () => {
+    const newTitle = document.querySelector('#title_input').value;
+    newTitle && setMusicList(prevMusicList => {
+      return (
+        [
+          ...prevMusicList,
+          {
+            id: new Date().getTime(),
+            title: newTitle,
+            like: 0
+          }
+        ]
+      )
+    })
+    document.querySelector('#title_input').value = "";
+  }
 
   return (
     <div className="App">
@@ -26,23 +45,7 @@ function App() {
         <div>
           <label htmlFor="title_input"></label>
           <input id='title_input' type='text' placeholder='음악 제목을 입력하세요' />
-          <button onClick={()=>{
-            const newTitle = document.querySelector('#title_input').value;
-
-            newTitle && setMusicList(prevMusicList => {
-              return (
-                [
-                  ...prevMusicList,
-                  {
-                    id: new Date().getTime(),
-                    title: newTitle,
-                    like: 0
-                  }
-                ]
-              )
-            })
-            document.querySelector('#title_input').value = "";
-          }}>추가</button>
+          <button onClick={addMusic}>추가</button>
         </div>
         {
           musicList.map((music, idx)=>{
--- 07_react/240411/music/src/components/Music.jsx
+++ 07_react/240411/music/src/components/Music.jsx
@@ -1,13 +1,34 @@
 const Music = ({ music, idx, musicList, setMusicList }) => {
+    const { id, title, like } = music;
+
+    const addLike = () => {
+        const copy = [...musicList];
+        copy[idx].like++;
+        setMusicList(copy);
+    }
+    const editMusic = () => {
+        const newTitle = prompt('새 제목을 입력하세요');
+        const copy = [...musicList]
+        copy[idx].title = newTitle;
+        setMusicList(copy);
+    }
+
+    const deleteMusic = () => {
+        const copy = musicList.filter((m) => {
+            return m.id != id
+        })
+        setMusicList(copy);
+    }
+
     return ( 
         <div>
-            <h2>{music.title}</h2>
-            <span style={{ "cursor": "pointer" }} onClick={() => {
-                const copy = [...musicList];
-                copy[idx].like++;
-                setMusicList(copy);
-            }}> 👍 </span>
-            <span> 좋아요 : {music.like}</span>
+            <h2>{title}</h2>
+            <div>
+                <span style={{ "cursor": "pointer" }} onClick={addLike}> 👍 </span>
+                <span> 좋아요 : {like}</span>
+            </div>
+            <button onClick={editMusic}>수정</button>
+            <button onClick={deleteMusic}>삭제</button>
         </div>
     );
 }
