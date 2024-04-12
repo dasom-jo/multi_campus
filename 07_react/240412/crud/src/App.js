84aa62b086d8662c81df033622e2caa813fd5d4e
Inkyu24 <484342@gmail.com>
2024-04-12 15:58:26 +0900

    a

--- 07_react/240412/crud/src/App.js
+++ 07_react/240412/crud/src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import Article from './components/layouts/Article';
 import Header from './components/layouts/Header';
 
 function App() {
+  const [mode, setMode] = useState("WELCOME");
+
+  let content = null;
+
+  if (mode === "WELCOME") {
+    content = <Article>환영합니다.</Article>;
+  }
   return (
     <div className="App">
       <Header></Header>
-      <Article></Article>
+      {content}
     </div>
   );
 }
