b846d15f48a7a7b9d997bfad45f2d2fef6e29a50
Inkyu24 <484342@gmail.com>
2024-04-15 11:48:47 +0900

    sports

--- 07_react/240415/hook_basic/src/components/News.jsx
+++ 07_react/240415/hook_basic/src/components/News.jsx
@@ -10,7 +10,7 @@ const News = () => {
 
     const getNews = async () => {
         try {
-            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=f047d047f72c46dbb1b0d9a47af199f3`)
+            const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&category=sports&apiKey=f047d047f72c46dbb1b0d9a47af199f3`)
             console.log(response.data.articles);
             setArticles(response.data.articles);
         } catch (err) {
