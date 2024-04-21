381974144158b6c781b31cf3c0ec65606358f032
jodasom <jodasom0216@gmail.com>
2024-04-22 00:28:35 +0900

    useinputs review

--- 240418/shoping_router/src/hooks/useInputs.jsx
+++ 240418/shoping_router/src/hooks/useInputs.jsx
@@ -8,13 +8,37 @@ const useInputs = (initialForm) => {
         setForm(form => ({...form, [name]: value}));
     });
 
+    // useEffect(()=>{
+    //     if(form.email && form.nickname && form.password && form.pwd_chk && (form.password === form.pwd_chk)){
+    //         setForm(form => ({...form, validate: true, errMsg:'  '}));
+    //     }else {
+    //         setForm(form=> ({...form, validate: false, errMsg:" 너입력덜햇어 "}));
+    //     }
+    // }, [form.email, form.nickname, form.password, form.pwd_chk]);
     useEffect(()=>{
-        if(form.email && form.nickname && form.password && form.pwd_chk && (form.password === form.pwd_chk)){
-            setForm(form => ({...form, validate: true, errMsg:''}));
-        }else {
-            setForm(form=> ({...form, validate: false, errMsg:"너입력덜햇어"}));
+        if(form.email && form.nickname && form.password && form.pwd_chk && (form.password === form.pwd_chk) ){
+            setForm(form => ({...form, validate: false, errMsg:'내용을 모두 입력하였습니다. 가입버튼을 눌러주세요'}));
+        }
+        else if(!form.email){
+            setForm(form => ({...form, validate: false, errMsg:'이메일을 입력해주세요'}));
+        }
+        else if(!form.nickname){
+            setForm(form => ({...form, validate: false, errMsg:'닉네임을 입력해주세요 '}));
+        }
+        else if(!form.password){
+            setForm(form => ({...form, validate: false, errMsg:'비밀번호를 입력해주세요'}));
+        }
+        else if(!form.pwd_chk){
+            setForm(form => ({...form, validate: false, errMsg:'비밀번호를 확인해주세요 '}));
+        }
+        else if(!(form.password === form.pwd_chk) ){
+            setForm(form => ({...form, validate: false, errMsg:'비밀번호가 다릅니다 '}));
+        }
+        else {
+            setForm(form=> ({...form, validate: false, errMsg:"모두 작성하셨습니다"}));
         }
     }, [form.email, form.nickname, form.password, form.pwd_chk]);
+
 
     const reset = useCallback(() => {
         setForm(initialForm)
