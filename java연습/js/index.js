6d571691ba6d815159c37b7d78778fc321cbfb50
jodasom <jodasom0216@gmail.com>
2024-07-16 11:30:21 +0900

    js study

--- java연습/js/index.js
+++ java연습/js/index.js
@@ -1,11 +1,29 @@
-function solution(str2, target) {
-    if(str2.includes(target)) {
-        return 1;
-    }else{
-    return 0;
-    }
+function test01(a,b){
+    return a * b;
 }
-let str1 = "abc"
-let str2 = 	"aabcc"
+console.log(test01(3,5));
 
-console.log(solution(str2, str2))
+//변수에담는 함수
+let test02 = function(a,b){
+    return a * b;
+}
+console.log(test02(3,5));
+
+//생성자함수
+let test03 = new Function('a','b','return03(3,5)')
+
+//화살표함수
+let test04=(a,b)=>{
+    return a*b;
+}
+console.log(test04(3,5));
+
+//즉시호출한수
+(function(){
+    console.log(3*5);
+})();
+
+const plus =(x=10,y=10)=>{
+    return x+y;
+}
+console.log(plus(0,1));
